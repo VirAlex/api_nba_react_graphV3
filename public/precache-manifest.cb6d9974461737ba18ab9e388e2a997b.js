db9cc9a288ed9b6c0edbe059f340ad6c17344417
VirAlex <alexou73@gmail.com>
2020-02-10 15:18:33 +0100

    deploy

---
+++ public/precache-manifest.cb6d9974461737ba18ab9e388e2a997b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35823d7cd0a3a07b7e98bee7193b0746",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa1e53e49a536636acb5",
+    "url": "/static/css/main.26a8c766.chunk.css"
+  },
+  {
+    "revision": "4e3a357394aa8cac5da0",
+    "url": "/static/js/2.3694cab1.chunk.js"
+  },
+  {
+    "revision": "8e845481c75d8335ada50d008284a93a",
+    "url": "/static/js/2.3694cab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1e53e49a536636acb5",
+    "url": "/static/js/main.7165de36.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "76a1d92f58cc6c982f9cdc2b5d21134c",
+    "url": "/static/media/logo.76a1d92f.png"
+  }
+]);
